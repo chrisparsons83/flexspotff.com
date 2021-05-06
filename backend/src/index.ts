a10dcbf944b7183825a92b45cc029c8247e00718
Chris Parsons <chrisparsons1127@gmail.com>
2021-05-05 21:43:02 -0700

    Get basic discord login working.

---
+++ backend/src/index.ts
@@ -0,0 +1,46 @@
+import dotenv from 'dotenv';
+import fastify from 'fastify';
+import oauthPlugin from 'fastify-oauth2';
+
+// Load env file first.
+dotenv.config();
+
+// Start fastify server.
+const server = fastify();
+
+server.register(oauthPlugin, {
+  name: 'discordOAuth2',
+  credentials: {
+    client: {
+      id: process.env.DISCORD_CLIENT_ID,
+      secret: process.env.DISCORD_CLIENT_SECRET,
+    },
+    auth: oauthPlugin.DISCORD_CONFIGURATION,
+  },
+  // register a fastify url to start the redirect flow
+  startRedirectPath: '/login/discord',
+  // facebook redirect here after the user login
+  callbackUri: 'http://localhost:8080/login/discord/callback',
+  scope: ['identify'],
+});
+
+// Leave this in as a bit of a smoke test for now.
+// TODO: Remove this when not needed anymore.
+server.get('/ping', async (request, reply) => {
+  return 'pong\n';
+});
+
+server.get('/login/discord/callback', async (request, reply) => {
+  console.log({ request }); // Just for debugging
+
+  // Redirect to a route serving HTML or to your front-end
+  reply.redirect('http://localhost:3000/?token=');
+});
+
+server.listen(8080, (err, address) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(`Server listening at ${address}`);
+});
