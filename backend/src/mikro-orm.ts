47daa091be40172cf10708a6a3ed1c1479d3998e
Chris Parsons <chrisparsons1127@gmail.com>
2021-06-13 21:17:41 -0700

    Update mikroorm module for better namespace.

--- backend/src/mikro-orm.ts
+++ backend/src/mikro-orm.ts
@@ -7,7 +7,7 @@ import config from './mikro-orm.config';
 
 declare module 'fastify' {
   interface FastifyInstance {
-    orm: MikroORM;
+    mikroorm: MikroORM;
   }
 }
 
@@ -16,11 +16,11 @@ export interface FastifyMikroORMPluginOptions {
 }
 
 const mikroOrmConnector: FastifyPluginAsync<FastifyMikroORMPluginOptions> = async (fastify) => {
-  const orm = await MikroORM.init(config);
+  const mikroorm = await MikroORM.init(config);
 
   // Decorate
-  fastify.decorate('orm', orm).addHook('onClose', async (instance) => {
-    await instance.orm.close();
+  fastify.decorate('mikroorm', mikroorm).addHook('onClose', async (instance) => {
+    await instance.mikroorm.close();
   });
 };
 
