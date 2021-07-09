da277c1301fe659f8191bea4e802b74e398ade08
Chris Parsons <chris@chris-parsons.com>
2021-07-08 21:35:55 -0700

    Clean up lint errors.

--- frontend/lib/useUser.tsx
+++ frontend/lib/useUser.tsx
@@ -2,10 +2,10 @@ import useSWR from 'swr';
 
 const useUser = () => {
   // const { data, error } = useSWR('http://localhost:8080/ping');
-  const {data, error} = {
-    data: "ping",
-    error: null
-  }
+  const { data, error } = {
+    data: 'ping',
+    error: null,
+  };
 
   return {
     data,
