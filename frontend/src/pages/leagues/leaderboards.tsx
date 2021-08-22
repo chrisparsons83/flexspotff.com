bb0cd9d6537cb79fe302dc6960a88dc92d81d784
Chris Parsons <chris@chris-parsons.com>
2021-08-21 23:04:30 -0700

    Sample styles for all leagues.

--- frontend/src/pages/leagues/leaderboards.tsx
+++ frontend/src/pages/leagues/leaderboards.tsx
@@ -22,7 +22,7 @@ const LeaderboardsPage = () => {
         </SimpleGrid>
         <SimpleGrid columns={[1, null, 2]} spacingX={10} spacingY={3} mb={2} p={2} bg="brand.300">
           <Flex>
-            <Box>1.</Box>
+            <Box>21.</Box>
             <Box>christhrowrocks</Box>
             <Spacer />
             <Box>
@@ -38,7 +38,39 @@ const LeaderboardsPage = () => {
         </SimpleGrid>
         <SimpleGrid columns={[1, null, 2]} spacingX={10} spacingY={3} mb={2} p={2} bg="brand.300">
           <Flex>
+            <Box>22.</Box>
+            <Box>Bootz</Box>
+            <Spacer />
+            <Box>
+              <LeagueName leagueName="Champions" />
+            </Box>
+          </Flex>
+          <Flex>
+            <Box w={1 / 4}>179.10</Box>
+            <Box w={1 / 4}>144.45</Box>
+            <Box w={1 / 4}>1221.10</Box>
+            <Box w={1 / 4}>1140.55</Box>
+          </Flex>
+        </SimpleGrid>
+        <SimpleGrid columns={[1, null, 2]} spacingX={10} spacingY={3} mb={2} p={2} bg="brand.300">
+          <Flex>
             <Box>23.</Box>
+            <Box>Smelscifi</Box>
+            <Spacer />
+            <Box>
+              <LeagueName leagueName="Dragon" />
+            </Box>
+          </Flex>
+          <Flex>
+            <Box w={1 / 4}>177.70</Box>
+            <Box w={1 / 4}>105.35</Box>
+            <Box w={1 / 4}>1099.90</Box>
+            <Box w={1 / 4}>1053.25</Box>
+          </Flex>
+        </SimpleGrid>
+        <SimpleGrid columns={[1, null, 2]} spacingX={10} spacingY={3} mb={2} p={2} bg="brand.300">
+          <Flex>
+            <Box>24.</Box>
             <Box>Noro</Box>
             <Spacer />
             <Box>
@@ -50,6 +82,22 @@ const LeaderboardsPage = () => {
             <Box w={1 / 4}>125.35</Box>
             <Box w={1 / 4}>1140.00</Box>
             <Box w={1 / 4}>1003.70</Box>
+          </Flex>
+        </SimpleGrid>
+        <SimpleGrid columns={[1, null, 2]} spacingX={10} spacingY={3} mb={2} p={2} bg="brand.300">
+          <Flex>
+            <Box>25.</Box>
+            <Box>Brando</Box>
+            <Spacer />
+            <Box>
+              <LeagueName leagueName="Monarch" />
+            </Box>
+          </Flex>
+          <Flex>
+            <Box w={1 / 4}>176.30</Box>
+            <Box w={1 / 4}>165.35</Box>
+            <Box w={1 / 4}>1177.75</Box>
+            <Box w={1 / 4}>1105.05</Box>
           </Flex>
         </SimpleGrid>
       </div>
