8906f216e19833b8926cf82616d28d696de5cf5f
Chris Parsons <chris@chris-parsons.com>
2021-08-22 21:16:41 -0700

    Change padding on league name.

--- frontend/src/features/leaderboard/components/LeagueName.tsx
+++ frontend/src/features/leaderboard/components/LeagueName.tsx
@@ -8,7 +8,14 @@ interface Props {
 const LeagueName: React.FC<Props> = ({ leagueName }) => {
   const leagueNameClass = `${leagueName.toLowerCase()}.500`;
   return (
-    <Text p={1} bg={leagueNameClass} fontSize="xs" fontWeight="bold" textTransform="uppercase">
+    <Text
+      px={1.5}
+      py={0.5}
+      bg={leagueNameClass}
+      fontSize="xs"
+      fontWeight="bold"
+      textTransform="uppercase"
+    >
       {leagueName}
     </Text>
   );
