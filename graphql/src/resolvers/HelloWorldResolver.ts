9ee224cffb961a14984cb24a20e0dd1a877c4e4b
Chris Parsons <chris@chris-parsons.com>
2021-09-13 08:56:18 -0700

    Change linter for type-graphql.

--- graphql/src/resolvers/HelloWorldResolver.ts
+++ graphql/src/resolvers/HelloWorldResolver.ts
@@ -3,7 +3,6 @@ import { Query, Resolver } from 'type-graphql';
 @Resolver()
 export default class HelloWorldResolver {
   @Query(() => String)
-  // eslint-disable-next-line class-methods-use-this
   hello(): string {
     return 'hi!';
   }
