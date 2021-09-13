001159321c8269133300accdd229c4bce73a3b62
Chris Parsons <chris@chris-parsons.com>
2021-09-12 22:23:30 -0700

    Get a basic GraphQL server going.

---
+++ graphql/src/index.ts
@@ -0,0 +1,32 @@
+import { ApolloServer, gql } from 'apollo-server-fastify';
+import fastify from 'fastify';
+
+// The GraphQL schema
+const typeDefs = gql`
+  type Query {
+    "A simple type for getting started!"
+    hello: String
+  }
+`;
+
+// A map of functions which return data for the schema.
+const resolvers = {
+  Query: {
+    hello: () => 'world',
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const app = fastify({
+  logger: true,
+});
+
+(async function serverStart() {
+  await server.start();
+  app.register(server.createHandler());
+  await app.listen(3000);
+})();
