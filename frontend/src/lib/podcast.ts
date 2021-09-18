2c8608a5e3bc4c88c0c2eeebcaef4bf282cbf188
Chris Parsons <chris@chris-parsons.com>
2021-09-17 22:10:43 -0700

    Fixing ownership.

--- frontend/src/lib/podcast.ts
+++ frontend/src/lib/podcast.ts
@@ -3,14 +3,18 @@ import { mkdirSync, writeFileSync } from 'fs';
 
 const feedOptions = {
   author: 'DrTrashdad and Bootzfantasy',
+  categories: ['Sports', 'Fantasy Sports'],
   copyright: 'Copyright 2021, DrTrashdad and Bootzfantasy',
   description:
     'Unleashed from the iconic Flexspot Fantasy Football Discord server, Dr. Trashdad and Bootz bring the next generation of a fantasy football podcast. A conversation between a numbers and spreadsheets analyst and a film room or gut drafter, Bootz and Trashdad talk about fantasy relevant topics to help you get the edge you need in your league. Redraft, dynasty, bestball, and even some sportsbetting are common topics. Listen to us to get in-depth and less talked-about fantasy knowledge without the fluff.',
   feedUrl: 'https://www.flexspotff.com/rss/trash-turtle-football.xml',
   imageUrl: 'http://flexspotff-podcast.ewr1.vultrobjects.com/ttf.jpg',
-  itunesCategory: ['Sports', 'Fantasy Sports'],
-  itunesExplicit: false,
   itunesAuthor: 'DrTrashdad and Bootzfantasy',
+  itunesExplicit: false,
+  itunesOwner: {
+    name: 'DrTrashdad and Bootzfantasy',
+    email: 'trashturtlefootball@flexspotff.com',
+  },
   itunesType: 'episodic',
   language: 'en-us',
   siteUrl: 'https://www.flexspotff.com/',
@@ -20,6 +24,7 @@ const feedOptions = {
 const feedEpisodes = [
   {
     description: `Bootz and trashdad return for Season 2! We discuss parts of the draft, Bootz's shower habits, CDCarter's intern bathroom habits, rookie feelings, best ball, draft ADP changes, Brando's milk tastes, Discord league announcements, and Over/Under hype reactions. We're so happy to be back and we're really hoping that you're reading this on Spotify or Apple podcasts!!!`,
+    duration: 2678,
     episode: 1,
     filepath: 'https://ewr1.vultrobjects.com/flexspotff-podcast/TTF_S02E01_-_The_Return.mp3',
     publishDate: '2021-08-31',
@@ -38,9 +43,10 @@ export const generatePodcastFeed = async () => {
       date: episode.publishDate,
       enclosure: {
         url: episode.filepath,
+        file: episode.filepath,
       },
       itunesAuthor: feedOptions.author,
-      itunesDuration: 2678,
+      itunesDuration: episode.duration,
       itunesTitle: episode.title,
       url: `https://www.flexspotff.com/podcast/${episode.season}/${episode.episode}`,
     };
