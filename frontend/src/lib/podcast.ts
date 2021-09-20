25f4c4e535be3cedf31bb4eab636dd7f4703388c
Chris Parsons <chris@chris-parsons.com>
2021-09-19 20:48:43 -0700

    S2E2 of podcast.

--- frontend/src/lib/podcast.ts
+++ frontend/src/lib/podcast.ts
@@ -33,6 +33,18 @@ const feedOptions = {
 
 const feedEpisodes = [
   {
+    description: `Bootz and trashdad take a look the flaming garbage heap that was a preseason and try to read the ashes like tea leaves to see what we can figure out for the upcoming 2021 season. DUME returns as our voiceover! We're glad to have him back. Get your kiddie pools back out because a.) summer isn't quite over yet and you still haven't fulfilled your dream of filling the entire thing with mojito and drinking yourself into nirvana and b.) these hot takes will make you want to drink a kiddie pool full of mojito. Still not as bad as Twitter. Enjoy!`,
+    duration: 3751,
+    episode: 2,
+    filepath: 'https://ewr1.vultrobjects.com/flexspotff-podcast/TTF_S02E02_-_2021_Hot_Takes.mp3',
+    filesize: 58347221,
+    filetype: 'audio/mpeg',
+    publishDate: '2021-09-06',
+    season: 2,
+    shownotes: '',
+    title: `2021 Hot Takes`,
+  },
+  {
     description: `Bootz and trashdad return for Season 2! We discuss parts of the draft, Bootz's shower habits, CDCarter's intern bathroom habits, rookie feelings, best ball, draft ADP changes, Brando's milk tastes, Discord league announcements, and Over/Under hype reactions. We're so happy to be back and we're really hoping that you're reading this on Spotify or Apple podcasts!!!`,
     duration: 2678,
     episode: 1,
