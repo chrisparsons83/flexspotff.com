69d72911a9f7a4b6b3211bd383b94e7d60c6bfd1
Chris Parsons <chris@chris-parsons.com>
2021-09-20 22:53:36 -0700

    Add update League resolver.

--- graphql/src/resolvers/LeagueResolver.ts
+++ graphql/src/resolvers/LeagueResolver.ts
@@ -1,4 +1,5 @@
 import { Arg, Ctx, Mutation, Query, Resolver } from 'type-graphql';
+import { wrap } from '@mikro-orm/core';
 import Axios from '../lib/axios';
 import { GraphQLContext, SleeperLeague } from '../types';
 import League from '../entities/League';
@@ -28,5 +29,25 @@ export default class LeagueResolver {
   async leagues(@Ctx() ctx: GraphQLContext): Promise<League[]> {
     const leagues = await ctx.em.find(League, {});
     return leagues;
+  }
+
+  @Mutation(() => League)
+  async updateLeague(
+    @Arg('sleeperLeagueId') sleeperLeagueId: string,
+    @Ctx() ctx: GraphQLContext,
+  ): Promise<League> {
+    const league = await ctx.em.findOneOrFail(League, { sleeperLeagueId });
+    const response = await Axios.get<SleeperLeague>(`/v1/league/${sleeperLeagueId}`);
+    const sleeperLeague = response.data;
+    const tier = sleeperLeague.name === 'Champions League' ? 1 : 2;
+    wrap(league).assign({
+      name: sleeperLeague.name,
+      season: sleeperLeague.season,
+      sleeperLeagueId: sleeperLeague.league_id,
+      sleeperDraftId: sleeperLeague.draft_id,
+      tier,
+    });
+    await ctx.em.persistAndFlush(league);
+    return league;
   }
 }
