c9c06168fd85162bf5f58f6041b5692e9e4d9130
Chris Parsons <chris@chris-parsons.com>
2021-09-27 21:42:27 -0700

    Adding expiration to cookie.

--- graphql/src/routes/auth.ts
+++ graphql/src/routes/auth.ts
@@ -63,6 +63,8 @@ const routes = async (fastify: FastifyInstance) => {
         .cookie('token', userToken, {
           domain: process.env.COOKIE_DOMAIN,
           path: '/',
+          httpOnly: true,
+          expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), // 7 days
         })
         .redirect(process.env.FRONTEND_URL);
     },
