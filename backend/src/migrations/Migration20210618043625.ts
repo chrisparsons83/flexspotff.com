6d80ed70a215d5025fc2308a7506432bb64e2b9a
Chris Parsons <chris@chris-parsons.com>
2021-09-28 13:11:09 -0700

    Remove old backend folder.

--- backend/src/migrations/Migration20210618043625.ts
+++
@@ -1,9 +0,0 @@
-import { Migration } from '@mikro-orm/migrations';
-
-export class Migration20210618043625 extends Migration {
-  async up(): Promise<void> {
-    this.addSql('alter table "league" add column "tier" int4 not null default 2;');
-  }
-}
-
-export default Migration20210618043625;
