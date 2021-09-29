8dfca2dc82c5d4220a005c3fbe60fb773157b112
Chris Parsons <chris@chris-parsons.com>
2021-09-28 20:11:06 -0700

    Revert "Change mikroorm to use entities folders."
    This reverts commit a2dd29c39567eb7716e25a9e584c95fdac9d5eb3.

--- graphql/src/mikro-orm.config.ts
+++ graphql/src/mikro-orm.config.ts
@@ -1,13 +1,17 @@
 import dotenv from 'dotenv';
 import { Options } from '@mikro-orm/core';
 
+import BaseEntity from './entities/BaseEntity';
+import League from './entities/League';
+import PodcastEpisode from './entities/PodcastEpisode';
+import User from './entities/User';
+
 dotenv.config();
 
 const config: Options = {
   dbName: process.env.DB_NAME,
   debug: true,
-  entities: ['dist/entities/**.js'],
-  entitiesTs: ['src/entities/**.ts'],
+  entities: [BaseEntity, League, PodcastEpisode, User],
   host: process.env.DB_HOST,
   migrations: {
     path: 'src/migrations/',
