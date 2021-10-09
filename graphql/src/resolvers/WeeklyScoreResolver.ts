e94269142b438be2d7e55428981da4c3880e4b06
Chris Parsons <chris@chris-parsons.com>
2021-10-08 21:43:20 -0700

    Adding weekly score update function.

--- graphql/src/resolvers/WeeklyScoreResolver.ts
+++ graphql/src/resolvers/WeeklyScoreResolver.ts
@@ -1,4 +1,5 @@
-import { Arg, Ctx, Mutation, Resolver } from 'type-graphql';
+import { wrap } from '@mikro-orm/core';
+import { Arg, Authorized, Ctx, Int, Mutation, Query, Resolver } from 'type-graphql';
 import League from '../entities/League';
 import Team from '../entities/Team';
 import Week from '../entities/Week';
@@ -8,15 +9,15 @@ import { GraphQLContext, SleeperMatchup } from '../types';
 
 @Resolver()
 export default class WeeklyScoreResolver {
+  @Authorized('ADMIN')
   @Mutation(() => Boolean)
   async createWeeklyScores(
-    @Arg('week') weekNumber: number,
+    @Arg('week', () => Int) weekNumber: number,
     @Ctx() ctx: GraphQLContext,
   ): Promise<boolean> {
     const leagues = await ctx.em.find(League, {});
     const week = await ctx.em.findOneOrFail(Week, { weekNumber });
     const teams = await ctx.em.find(Team, {});
-    console.log(teams);
 
     for (const league of leagues) {
       const { sleeperLeagueId } = league;
@@ -41,5 +42,58 @@ export default class WeeklyScoreResolver {
     await ctx.em.flush();
 
     return true;
+  }
+
+  @Authorized('ADMIN')
+  @Mutation(() => Boolean)
+  async updateWeeklyScores(
+    @Arg('week', () => Int) weekNumber: number,
+    @Ctx() ctx: GraphQLContext,
+  ): Promise<boolean> {
+    const leagues = await ctx.em.find(League, {});
+
+    for (const league of leagues) {
+      const { sleeperLeagueId } = league;
+
+      // eslint-disable-next-line no-await-in-loop
+      const responseMatchups = await Axios.get<SleeperMatchup[]>(
+        `/v1/league/${sleeperLeagueId}/matchups/${weekNumber}`,
+      );
+      const matchups = responseMatchups.data;
+
+      for (const matchup of matchups) {
+        // Get current matchup
+        // eslint-disable-next-line no-await-in-loop
+        const weeklyScore = await ctx.em.findOneOrFail(
+          WeeklyScore,
+          {
+            team: {
+              rosterId: matchup.roster_id,
+              league: {
+                sleeperLeagueId,
+              },
+            },
+            week: {
+              weekNumber,
+            },
+          },
+          ['team'],
+        );
+        wrap(weeklyScore).assign({ points: matchup.points });
+        ctx.em.persist(weeklyScore);
+      }
+    }
+    await ctx.em.flush();
+
+    return true;
+  }
+
+  @Query(() => [WeeklyScore])
+  async weeklyScores(
+    @Arg('week', () => Int) weekNumber: number,
+    @Ctx() ctx: GraphQLContext,
+  ): Promise<WeeklyScore[]> {
+    const weeklyScores = await ctx.em.find(WeeklyScore, { week: { weekNumber } }, ['team.league']);
+    return weeklyScores;
   }
 }
