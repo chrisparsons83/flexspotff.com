9ad9688e2588ed42bdaac5be7885005fbbe8c1fa
Chris Parsons <chris@chris-parsons.com>
2021-11-23 09:37:38 -0800

    Temp token time change.

--- graphql/src/routes/auth.ts
+++ graphql/src/routes/auth.ts
@@ -56,13 +56,13 @@ const routes = async (fastify: FastifyInstance) => {
 
       // Log user in by creating JWT
       // eslint-disable-next-line no-underscore-dangle
-      const userToken = sign({ userId: localUser._id }, process.env.JWT_KEY, { expiresIn: '7d' });
+      const userToken = sign({ userId: localUser._id }, process.env.JWT_KEY, { expiresIn: '365d' });
 
       // Redirect to homepage
       reply
         .cookie('token', userToken, {
           domain: process.env.COOKIE_DOMAIN,
-          expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), // 7 days
+          expires: new Date(Date.now() + 365 * 24 * 60 * 60 * 1000), // 7 days
           httpOnly: true,
           path: '/',
           secure: true,
