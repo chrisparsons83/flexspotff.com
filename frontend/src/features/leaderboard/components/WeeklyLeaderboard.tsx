c26312419a8985a2855d757e0289078a2f5a9f5d
Chris Parsons <chris@chris-parsons.com>
2021-12-09 19:28:18 -0800

    Update to week 14

--- frontend/src/features/leaderboard/components/WeeklyLeaderboard.tsx
+++ frontend/src/features/leaderboard/components/WeeklyLeaderboard.tsx
@@ -5,7 +5,7 @@ import { WeeklyLeaderboardTable } from '..';
 import useWeeklyLeaderboard from '../hooks/useWeeklyLeaderboard';
 
 const WeeklyLeaderboard = () => {
-  const [week, setWeek] = useState(13);
+  const [week, setWeek] = useState(14);
   const { status, data, error, isFetching } = useWeeklyLeaderboard({ week });
 
   const weeksArray = Array.from({ length: 17 }, (_x, i) => i + 1);
